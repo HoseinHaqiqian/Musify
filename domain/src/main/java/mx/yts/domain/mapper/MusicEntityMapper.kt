7e61be2d2132e18fce7e22a240c2dee3a95fd1e1
Hosein Haqiqian <hosein.haqiqian@wallit.eu>
2023-03-08 18:09:18 +0330

    created mapper for music entity

--- domain/src/main/java/mx/yts/domain/mapper/MusicEntityMapper.kt
+++ domain/src/main/java/mx/yts/domain/mapper/MusicEntityMapper.kt
@@ -1,12 +1,28 @@
 package mx.yts.domain.mapper
 
 import mx.yts.data.entities.MusicEntity
+import mx.yts.domain.model.DurationModel
 import mx.yts.domain.model.MusicModel
 import javax.inject.Inject
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.DurationUnit
 
 class MusicEntityMapper @Inject constructor() {
     fun mapTo(from: MusicEntity): MusicModel {
-        return MusicModel(from.id, from.name, artist = from.artist, artPat = from.artUri)
+        val milliseconds = from.duration.toLong()
+        val minutes = (milliseconds / 1000 / 60).toInt()
+        val seconds = (milliseconds / 1000 % 60).toInt()
+        val inSeconds = milliseconds.milliseconds.inWholeSeconds
+        return MusicModel(
+            from.id,
+            from.name,
+            artist = from.artist,
+            songUri = from.songUri,
+            artUrl = from.artUrl,
+            duration = DurationModel(minutes,seconds,inSeconds),
+            sArtworkUri = from.sArtworkUri
+        )
     }
 
 }
