5369998512e7c43f4f3c9bf8bad7a848285ef8c3
Hosein Haqiqian <hosein.haqiqian@wallit.eu>
2023-03-08 18:05:02 +0330

    created model for ui

---
+++ domain/src/main/java/mx/yts/domain/model/DurationModel.kt
@@ -0,0 +1,3 @@
+package mx.yts.domain.model
+
+data class DurationModel(val minutes: Int, val seconds: Int, val inWholeSeconds: Long)
--- domain/src/main/java/mx/yts/domain/model/MusicModel.kt
+++ domain/src/main/java/mx/yts/domain/model/MusicModel.kt
@@ -3,8 +3,11 @@ package mx.yts.domain.model
 import android.net.Uri
 
 class MusicModel(
-    val id:Int?,
-    val name:String?,
-    val artPat : Uri,
-    val artist : String,
+    var id: Int,
+    var name: String,
+    var artist: String,
+    var artUrl: Uri,
+    var songUri: String,
+    val sArtworkUri: Uri,
+    val duration: DurationModel
 )
