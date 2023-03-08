c51ef5013f835fc528e6e0d498aa58c76fca6940
Hosein Haqiqian <hosein.haqiqian@wallit.eu>
2023-03-08 18:11:28 +0330

    moved music list view model

--- musiclist/src/main/java/mx/yts/musiclist/viewmodels/MusicListViewModel.kt
+++ musiclist/src/main/java/mx/yts/musiclist/viewmodels/MusicListViewModel.kt
@@ -1,16 +1,12 @@
-package mx.yts.musiclist.presentation.viewmodels
+package mx.yts.musiclist.viewmodels
 
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import dagger.assisted.Assisted
-import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import mx.yts.common.Result
+import mx.yts.common.utils.Result
 import mx.yts.domain.model.MusicModel
 import mx.yts.domain.usecases.GetMusicListUseCase
 import javax.inject.Inject
@@ -20,9 +16,12 @@ class MusicListViewModel @Inject constructor(
     private var getMusicListUseCase: GetMusicListUseCase
 ) :
     ViewModel() {
+
     var counter = 0
+
     private val musicsState =
         MutableStateFlow<Result<List<MusicModel>>>(Result.Success(emptyList()))
+
     val musicUiState: StateFlow<Result<List<MusicModel>>> = musicsState
 
     fun getMusics() {
