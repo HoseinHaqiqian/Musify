2c962222288cc7c8a42efb2ae20569a644deb0e4
Hosein Haqiqian <hosein.haqiqian@wallit.eu>
2023-03-08 18:05:20 +0330

    updated sdk version

--- buildSrc/src/main/kotlin/GradleConfigs.kt
+++ buildSrc/src/main/kotlin/GradleConfigs.kt
@@ -1,9 +1,10 @@
 object GradleConfigs {
     const val applicationId = "mx.yts.movix"
-    const val compileSdkVersion = 31
+    const val compileSdkVersion = 33
+    const val compileSdkVersionString = "android-33"
     const val buildToolsVersion = "30.0.3"
     const val minSdkVersion = 21
-    const val targetSdkVersion = 30
+    const val targetSdkVersion = 33
     const val versionCode = 1
     const val versionName = "1.0"
 }
