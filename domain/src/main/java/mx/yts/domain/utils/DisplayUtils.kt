9d9bad3aeff184432062fbaaade975c9cbddb246
Hosein Haqiqian <hosein.haqiqian@wallit.eu>
2023-03-09 12:43:54 +0330

    added twoDigitFormat and formattedMusicTime methods

--- domain/src/main/java/mx/yts/domain/utils/DisplayUtils.kt
+++ domain/src/main/java/mx/yts/domain/utils/DisplayUtils.kt
@@ -3,12 +3,22 @@ package mx.yts.domain.utils
 import android.content.res.Configuration
 import android.util.DisplayMetrics
 import android.util.Size
+import mx.yts.domain.model.DurationModel
+import mx.yts.domain.model.MusicModel
 
 class DisplayUtils {
 
     companion object {
         fun getScreenSize(configuration: Configuration): Size {
             return Size(configuration.screenWidthDp, configuration.screenHeightDp)
+        }
+
+        fun twoDigitFormat(value: Int): String {
+            return if (value < 10) "0${value}" else value.toString()
+        }
+
+        fun formattedMusicTime(duration: DurationModel):String{
+            return "${twoDigitFormat(duration.minutes)}:${twoDigitFormat(duration.seconds)}"
         }
     }
 
